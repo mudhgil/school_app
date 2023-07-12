ea1e989bb8740a3f507f2168bd7db08feb1faaec
mudhgil <vikasmudhgil@gmail.com>
2023-07-13 02:08:09 +0530

    css updated

--- SchoolDB/schooladmin/schooladmin/urls.py
+++ SchoolDB/schooladmin/schooladmin/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('delete/<int:pk>', views.DeleteSchoolView.as_view(), name = 'delete_school'),
     path('detail/<int:pk>', views.SchoolDetailView.as_view(), name = 'detail'),
     path('login/',auth_views.LoginView.as_view(),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page=''), name='logout'),
     
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
